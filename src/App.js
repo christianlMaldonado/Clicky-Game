26f9627aefae9fcba9cb7d7dab599ed764265f7d
Christian Maldonado <41269975+christianlMaldonado@users.noreply.github.com>
2021-01-07 13:56:32 -0800

    corrections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import GridMDC from "./Components/GridMDC";
-import PaperMDC from "./Components/PaperMDC";
-import CharCard from "./Components/CharCard";
-import Score from "./Components/Score";
-import Alert from "./Components/Alert";
-import NavBar from "./Components/NavBar";
-import BottomNavMDC from "./Components/BottomNavMDC";
+import GridMDC from "./components/GridMDC";
+import PaperMDC from "./components/PaperMDC";
+import CharCard from "./components/CharCard";
+import Score from "./components/Score";
+import Alert from "./components/Alert";
+import NavBar from "./components/NavBar";
+import BottomNavMDC from "./components/BottomNavMDC";
 import characters from "./characters.json";
 
 class App extends Component {
